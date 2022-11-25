076cd3fe053ce6febc508bf416f9443cded804a0
Ayush Kavad <ayushkavad@gmail.com>
2022-11-25 23:54:53 +0530

    deploy to production

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = "76925400-1052-48da-b2eb-4e05b8a41ade";
 export const MODAL_CLOSE_SEC = 2.5;
